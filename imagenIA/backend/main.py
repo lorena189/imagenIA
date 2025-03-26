2e62da9cd7a40de820509291252418c982dfaa98
Natali Davila <natalidavila52@gmail.com>
2025-03-26 14:38:18 -0600

    corrección de codigo

--- imagenIA/backend/main.py
+++ imagenIA/backend/main.py
@@ -6,8 +6,7 @@ import base64
 from flask_cors import CORS
 
 app = Flask(__name__)
-
-CORS(app, resources={r"/": {"origins": ""}})
+CORS(app, resources={r"/transform": {"origins": "http://localhost:5173"}})
 
 def transform_image(url):
     try:
@@ -47,5 +46,6 @@ def image_result():
 
     return jsonify(response), status_code
 
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
+
